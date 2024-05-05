39122cce752938f8151330ee30d7a8ba6015dfc3
ruselseo <ruselseo@gmail.com>
2024-05-05 11:29:12 +0600

    Add Dao interface

---
+++ src/main/dao/Dao.java
@@ -0,0 +1,19 @@
+package main.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao <F, T>{
+
+
+    List<T> findAll();
+
+    Optional<T> findById(Integer id);
+
+    boolean delete(F id);
+
+    void update(T entity);
+
+    T save(T entity);
+
+}
