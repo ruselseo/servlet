a7d16c7a76326b531c695e4460f75492465fa399
ruselseo <ruselseo@gmail.com>
2024-05-05 13:07:35 +0600

    Complete ConnectionManager class

--- src/main/util/ConnectionManager.java
+++ src/main/util/ConnectionManager.java
@@ -1,6 +1,5 @@
 package main.util;
 
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,8 +8,18 @@ public final class ConnectionManager {
 
     private ConnectionManager() {}
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("url", "user", "password" );
+    private static String URL = "db_url";
+    private static String USER = "db_user";
+    private static String PASSWORD = "db_password";
+
+    public static Connection get() throws SQLException {
+        try {
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL),
+                    PropertiesUtil.get(USER),
+                    PropertiesUtil.get(PASSWORD));
+        }catch (SQLException throwable) {
+            throw new RuntimeException(throwable);}
     }
 
 
