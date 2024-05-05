143fabdae395da740bb03a5b7e03296bbb455c6c
ruselseo <ruselseo@gmail.com>
2024-05-05 12:17:34 +0600

    Add User class

---
+++ src/main/entity/User.java
@@ -0,0 +1,13 @@
+package main.entity;
+
+import java.time.LocalDate;
+
+public class User {
+    private Integer id;
+    private String name;
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private Role role;
+    private Gender gender;
+}
